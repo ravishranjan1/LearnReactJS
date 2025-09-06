4317214ee0aab0ba3dcc7d21830bd0c37ae322fa
ravishranjan1 <161034433+ravishranjan1@users.noreply.github.com>
2025-09-06 22:08:16 +0530

    Start to Learn Reactjs

---
+++ src/Ravish.js
@@ -0,0 +1,9 @@
+function Ravish() {
+  return (
+    <div className="Ravish">
+      <h1>Hello Ravish Ranjan !</h1>
+    </div>
+  );
+}
+
+export default Ravish;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Ravish from './Ravish';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Ravish />    
+  </React.StrictMode>
+);
+//Use Ravish in place of App in line no 11
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
