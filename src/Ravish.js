d7a355ac69f7cd748bf835492ef4743041de47e8
Ravish <ravishranjan.rr@gmail.com>
2025-09-07 12:48:14 +0530

    Add class Component and make Apple component inside Ravish component

--- src/Ravish.js
+++ src/Ravish.js
@@ -1,9 +1,19 @@
 import User from './User'
 function Ravish() {
+
+  function Apple(){
+    return(
+      <div>
+        <h6>Apple Component</h6>
+      </div>
+    );
+  }
+
   return (
     <div className="Ravish">
       <h1>Hello Ravish !</h1>
        <User />
+       <Apple />
     </div>
   );
 }
--- src/User.js
+++ src/User.js
@@ -1,10 +1,28 @@
-function User()
-{
-    return(
-        <div>
-            <h1>User Component</h1>
-        </div>
+// //Functional Components
+// function User()
+// {
+//     return(
+//         <div>
+//             <h1>Functional Component</h1>
+//             <h2>hello</h2>
+//         </div>
         
-    );
+//     );
+// }
+// export default User;
+
+// Class Components
+import React,{Component} from 'react'
+class User extends Component
+{
+    render()
+    {
+        return(
+            <div>
+                <h1>Class Component</h1>
+            <h2> Hello </h2>
+            </div>
+        );
+    }
 }
 export default User;
