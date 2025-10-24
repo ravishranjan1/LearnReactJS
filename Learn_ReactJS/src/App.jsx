f55cb7668545c0a4dea7517b0f212f34823760d3
Ravish <ravishranjan.rr@gmail.com>
2025-10-24 12:54:41 +0530

    create first file

--- Learn_ReactJS/src/App.jsx
+++ Learn_ReactJS/src/App.jsx
@@ -1,8 +1,10 @@
+import Header from "./Header"
 function Apple(){
   return(
     <div>
+      <Header />
       <h1>Hello World !</h1>
-      <h2></h2>
+      <h2>Ravish Ranjan</h2>
     </div>
   )
 }
---
+++ Learn_ReactJS/src/Header.jsx
@@ -0,0 +1,8 @@
+function Header(){
+    return(
+        <div>
+            <h4>Learn With Code Step By Step</h4>
+        </div>
+    )
+}
+export default Header
