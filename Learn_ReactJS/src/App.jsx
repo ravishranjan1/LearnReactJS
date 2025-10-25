2da09d93c55f9730c0bdb6bd12f43c1a2d459c88
Ravish <ravishranjan.rr@gmail.com>
2025-10-25 22:29:19 +0530

    Importing and Exporting Components

--- Learn_ReactJS/src/App.jsx
+++ Learn_ReactJS/src/App.jsx
@@ -1,32 +1,15 @@
-import Head from './Header'
+import Login,{Profile,Setting,UserKey} from './UseComponents'
+
 function App(){
   return(
     <div>
-      <h1>First Component</h1>
-      <Fruit />
+      <h1>Importing Exporting Components</h1>
+      <Login />
+      <Setting/>
+      <Profile />
+      <h1>{UserKey}</h1>
     </div>
   )
 }
-
-function Fruit(){
-  return(
-    <div>
-      < Head/>
-      <h1>Apple</h1>
-      <h2>Banana</h2>
-      <Color />
-    </div>
-  )
-}
-
-function Color(){
-  return(
-    <div>
-      <h1>Red</h1>
-      <h4>Blue</h4>
-    </div>
-  )
-}
-
 
 export default App
---
+++ Learn_ReactJS/src/UseComponents.jsx
@@ -0,0 +1,25 @@
+function Login(){
+    return(
+        <div>
+            <h1>Login user</h1>
+        </div>
+    )
+}
+export default Login
+
+export function Profile(){
+    return(
+        <div>
+            <h1>Profile</h1>
+        </div>
+    )
+}
+export function Setting(){
+    return(
+        <div>
+            <h3>Setting</h3>
+        </div>
+    )
+}
+
+export const UserKey = "#@#@#@#@#";
