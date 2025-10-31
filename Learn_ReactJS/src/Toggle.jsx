6e97dc1ac6a55bf46b64077147135b35c3de482d
Ravish <ravishranjan.rr@gmail.com>
2025-10-31 23:23:48 +0530

    Multiple Condition

--- Learn_ReactJS/src/Toggle.jsx
+++ Learn_ReactJS/src/Toggle.jsx
@@ -1,15 +1,38 @@
 import { useState } from "react";
-function User(){
-    const [display, setDisplay] = useState(true);
-    return (
+//Toggle
+// function User(){
+//     const [display, setDisplay] = useState(true);
+//     return (
+//         <>
+//             <h1>Toggle in React JS</h1>
+//             <button onClick = {()=>setDisplay(!display)}>Toggle</button>
+//             {
+//                 display?<h1>Ravish Ranjan</h1>:null
+//             }
+            
+//         </>
+//     )
+// }
+// export default User;
+
+
+//Multiple Condition
+function user(){
+    const [count, setCount]= useState(0);
+
+    return(
         <>
-            <h1>Toggle in React JS</h1>
-            <button onClick = {()=>setDisplay(!display)}>Toggle</button>
+            <h1>{count}</h1>
+            <button onClick={()=>setCount(count+1)}>Counter</button>
             {
-                display?<h1>Ravish Ranjan</h1>:null
+                count==0?<h1>Condition0</h1>:
+                count==1?<h1>Condition1</h1>:
+                count==2?<h1>Condition2</h1>:
+                count==3?<h1>Condition3</h1>:
+                <h1>More Condition</h1>
             }
             
         </>
     )
 }
-export default User;
+export default user;
