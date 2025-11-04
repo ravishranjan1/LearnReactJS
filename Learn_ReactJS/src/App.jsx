42ce3b2deff8bca68bbae596506532bcf191a68c
Ravish <ravishranjan.rr@gmail.com>
2025-11-05 01:15:46 +0530

    Input Fields

--- Learn_ReactJS/src/App.jsx
+++ Learn_ReactJS/src/App.jsx
@@ -1,17 +1,12 @@
-
-
-import Wrapper from "./Wrapper"
-
+import{useState} from "react";
 function App(){
-  
+  const [val1,setVal1] = useState("");
   return(
     <>
-      <h1>Props in ReactJS</h1>
-      <Wrapper/>
-      <Wrapper color="orange"><h1>Hello</h1></Wrapper>
-      <Wrapper><h1>Hello</h1>
-        <h2 style={{color:"red"}}>hey</h2>
-      </Wrapper>
+      <h1>Get Input Fields value in ReactJS</h1>
+      <input type="text" value = {val1} onChange ={(event)=>setVal1(event.target.value)} placeholder = "Enter name"/>
+      <h1>{val1}</h1>
+      <button onClick={()=>setVal1("")}>Clear</button>
     </>
   )
 }
