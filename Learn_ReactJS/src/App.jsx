87a138c195b29799183fa106edae44a8645f4b97
Ravish <ravishranjan.rr@gmail.com>
2025-11-05 11:34:18 +0530

    Controlled Component

--- Learn_ReactJS/src/App.jsx
+++ Learn_ReactJS/src/App.jsx
@@ -1,12 +1,24 @@
 import{useState} from "react";
 function App(){
-  const [val1,setVal1] = useState("");
+  const [name, setName]=useState("");
+  const [password, setPassword]=useState("");
+  const [email, setEmail]=useState("");
   return(
     <>
-      <h1>Get Input Fields value in ReactJS</h1>
-      <input type="text" value = {val1} onChange ={(event)=>setVal1(event.target.value)} placeholder = "Enter name"/>
-      <h1>{val1}</h1>
-      <button onClick={()=>setVal1("")}>Clear</button>
+      <h1>Controlled Component</h1>
+      <form action="">
+        <input type="text" value={name} onChange={(event)=>{setName(event.target.value)}} placeholder="Enter name" /><br /><br />
+        <input type="password" value={password} onChange={(event)=>{setPassword(event.target.value)}} placeholder="Enter password" /><br /><br />
+        <input type="text" value={email} onChange={(event)=>{setEmail(event.target.value)}} placeholder="Enter Email" /><br /><br />
+        
+        <button>Submit</button>
+        <button onClick={()=>{setName("");setPassword("");setEmail("")}}>Clear</button>
+
+        <h1>{name}</h1>
+        <h1>{password}</h1>
+        <h1>{email}</h1>
+
+      </form>
     </>
   )
 }
