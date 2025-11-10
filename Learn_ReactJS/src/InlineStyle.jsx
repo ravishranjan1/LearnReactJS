5094f4d7a550bb9c8f1ce10d466215cabc55de71
Ravish <ravishranjan.rr@gmail.com>
2025-11-10 22:47:04 +0530

    Dynamic Style

--- Learn_ReactJS/src/InlineStyle.jsx
+++ Learn_ReactJS/src/InlineStyle.jsx
@@ -1,70 +1,91 @@
+import {useState} from "react"
 function Inline() {
-    const cardStyle = {
-        border: '2px solid black',
-        padding: '20px',
-        width: '150px',
-        borderRadius: '20px',
-        textAlign: 'center',
-        boxShadow: '2px 2px 2px 2px #e5d0d0ff'
-    }
+    const [textColor,setTextColor]=useState('red')
+    const[cardStyle,setCardStyle]=useState(
+       {
+            border: '2px solid black',
+            padding: '20px',
+            width: '150px',
+            borderRadius: '20px',
+            textAlign: 'center',
+            boxShadow: '2px 2px 2px 2px #e5d0d0ff',
+        }
+    )
+    
     const imgStyle = {
         height: "180px",
         width: '200px',
         marginLeft: '-30px'
     }
-    const dis = {
-        display: 'flex',
-        gap: '20px',
-        flexWrap: 'wrap'
+    const [grid, setGrid]=useState(true)
+    
+    const updateTheme=(bgColor,textColor)=>{
+        setCardStyle({...cardStyle,backgroundColor:bgColor})
+        setTextColor(textColor)
     }
+   
     return (
         <div>
-            <h1 style={{ color: 'red' }}>Inline Style in ReactJS</h1>
-            <div style={dis}>
+            <h1 style={{ color: 'red' }}>Dynamic & Conditional Style in ReactJS</h1>
+            <button onClick={()=>updateTheme('grey','green')}>Grey Theme</button>
+            <button onClick={()=>updateTheme('white','black')}>Default Theme</button>
+            <button onClick={()=>setGrid(!grid)}>Toggle Grid</button>
+            <div style={{display:grid?'flex':'block',flexWrap:'wrap'}}>
                 <div style={cardStyle}>
                     <img style={imgStyle} src="https://documents.iplt20.com/ipl/IPLHeadshot2025/2.png" alt="" />
-                    <h4>Virat Kholi</h4>
-                    <p>Indian Cricketer</p>
+                    <div style={{color:textColor}}>
+                        <h4>Virat Kholi</h4>
+                        <p>Indian Cricketer</p>
+                    </div>
                 </div>
-                <div style={cardStyle}>
+                 <div style={cardStyle}>
                     <img style={imgStyle} src="https://documents.iplt20.com/ipl/IPLHeadshot2025/2.png" alt="" />
-                    <h4>Virat Kholi</h4>
-                    <p>Indian Cricketer</p>
+                    <div style={{color:textColor}}>
+                        <h4>Virat Kholi</h4>
+                        <p>Indian Cricketer</p>
+                    </div>
                 </div>
-                <div style={cardStyle}>
+                 <div style={cardStyle}>
                     <img style={imgStyle} src="https://documents.iplt20.com/ipl/IPLHeadshot2025/2.png" alt="" />
-                    <h4>Virat Kholi</h4>
-                    <p>Indian Cricketer</p>
+                    <div style={{color:textColor}}>
+                        <h4>Virat Kholi</h4>
+                        <p>Indian Cricketer</p>
+                    </div>
                 </div>
-                <div style={cardStyle}>
+                 <div style={cardStyle}>
                     <img style={imgStyle} src="https://documents.iplt20.com/ipl/IPLHeadshot2025/2.png" alt="" />
-                    <h4>Virat Kholi</h4>
-                    <p>Indian Cricketer</p>
+                    <div style={{color:textColor}}>
+                        <h4>Virat Kholi</h4>
+                        <p>Indian Cricketer</p>
+                    </div>
                 </div>
-                <div style={cardStyle}>
+                 <div style={cardStyle}>
                     <img style={imgStyle} src="https://documents.iplt20.com/ipl/IPLHeadshot2025/2.png" alt="" />
-                    <h4>Virat Kholi</h4>
-                    <p>Indian Cricketer</p>
+                    <div style={{color:textColor}}>
+                        <h4>Virat Kholi</h4>
+                        <p>Indian Cricketer</p>
+                    </div>
                 </div>
-                <div style={cardStyle}>
+                 <div style={cardStyle}>
                     <img style={imgStyle} src="https://documents.iplt20.com/ipl/IPLHeadshot2025/2.png" alt="" />
-                    <h4>Virat Kholi</h4>
-                    <p>Indian Cricketer</p>
+                    <div style={{color:textColor}}>
+                        <h4>Virat Kholi</h4>
+                        <p>Indian Cricketer</p>
+                    </div>
                 </div>
-                <div style={cardStyle}>
+                 <div style={cardStyle}>
                     <img style={imgStyle} src="https://documents.iplt20.com/ipl/IPLHeadshot2025/2.png" alt="" />
-                    <h4>Virat Kholi</h4>
-                    <p>Indian Cricketer</p>
+                    <div style={{color:textColor}}>
+                        <h4>Virat Kholi</h4>
+                        <p>Indian Cricketer</p>
+                    </div>
                 </div>
-                <div style={cardStyle}>
+                 <div style={cardStyle}>
                     <img style={imgStyle} src="https://documents.iplt20.com/ipl/IPLHeadshot2025/2.png" alt="" />
-                    <h4>Virat Kholi</h4>
-                    <p>Indian Cricketer</p>
-                </div>
-                <div style={cardStyle}>
-                    <img style={imgStyle} src="https://documents.iplt20.com/ipl/IPLHeadshot2025/2.png" alt="" />
-                    <h4>Virat Kholi</h4>
-                    <p>Indian Cricketer</p>
+                    <div style={{color:textColor}}>
+                        <h4>Virat Kholi</h4>
+                        <p>Indian Cricketer</p>
+                    </div>
                 </div>
             </div>
         </div>
